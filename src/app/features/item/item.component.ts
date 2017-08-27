7370910588a6ec509d976e068641b0bc1a7a84b2
Ederson Abreu <ederson.dimm@gmail.com>
2017-08-27 09:24:11 -0300

    implementaçao da lista de itens

--- src/app/features/item/item.component.ts
+++ src/app/features/item/item.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
+
+import { Item } from './item';
+import { ItemService } from './item.service';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  data: Item[] = [];
+  filterQuery: string = '';
+  sub: Subscription;
+
+  constructor(
+    private itemService: ItemService
+  ) { }
 
   ngOnInit() {
+    this.list();
+  }
+
+  ngOnDestroy() {
+    if(this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
+
+  list() {
+    this.sub = this.itemService.list().subscribe(
+      itens => this.data = itens,
+      error => alert('Erro ao carregar lista de itens')
+    );
   }
 
 }
--- src/app/features/item/item.module.ts
+++ src/app/features/item/item.module.ts
@@ -1,29 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { DataTableModule } from 'angular2-datatable';
 import { TextMaskModule } from 'angular2-text-mask';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
 
 import { ItemComponent } from './item.component';
 import { ItemService } from './item.service';
+import { ItemFilterPipe } from './item-filter.pipe';
 import { FormItemComponent } from './form-item/form-item.component';
 import { ItemRoutingModule } from './item.routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     HttpModule,
+    DataTableModule,
     TextMaskModule,
     CurrencyMaskModule,
     ItemRoutingModule
   ],
   declarations: [
     ItemComponent,
+    ItemFilterPipe,
     FormItemComponent
   ],
   providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR'},
     ItemService
   ]
 })
